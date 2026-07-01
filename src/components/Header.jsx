4aed03fb2c3fbb50cf26e00f76a34937b3b2f344
Emanuele Favero <emafaux89@gmail.com>
2026-07-01 09:15:02 +0200

    style: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
+import logoImg from '../assets/img/dc-logo.png';
 import './Header.css';
 
 const navLinks = [
   { label: 'Characters', href: '#' },
-  { label: 'Comics', href: '#' },
+  { label: 'Comics', href: '#', isActive: true },
   { label: 'Movies', href: '#' },
   { label: 'TV', href: '#' },
   { label: 'Games', href: '#' },
@@ -13,8 +14,34 @@ const navLinks = [
   { label: 'Shop', href: '#' },
 ];
 
+const Logo = () => (
+  <a href='#' className='logo'>
+    <img src={logoImg} alt='DC Logo' width={80} height={80} />
+  </a>
+);
+
+const Navbar = () => (
+  <nav className='navbar'>
+    <ul>
+      {navLinks.map((navLink) => (
+        <li key={navLink.label}>
+          <a
+            href={navLink.href}
+            className={`uppercase text-xs font-bold ${navLink.isActive ? 'active' : ''}`}
+          >
+            {navLink.label}
+          </a>
+        </li>
+      ))}
+    </ul>
+  </nav>
+);
+
 export const Header = () => (
   <header className='header'>
-    <div className='container'>Header</div>
+    <div className='container'>
+      <Logo />
+      <Navbar />
+    </div>
   </header>
 );
