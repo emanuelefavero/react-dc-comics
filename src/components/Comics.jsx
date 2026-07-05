8763e9af78c42a0099f0e54179dbbc74e7970e8a
Emanuele Favero <emafaux89@gmail.com>
2026-07-05 05:58:21 +0200

    refactor: move comics into a separate component

---
+++ src/components/Comics.jsx
@@ -0,0 +1,27 @@
+import { comics } from '../data/comics.js';
+import './Comics.css';
+
+export const Comics = () => (
+  <section className='comics'>
+    <div className='container'>
+      <h1 className='title uppercase text-2xl'>Current Series</h1>
+
+      <ul className='list'>
+        {comics.map((comic) => (
+          <li key={comic.id}>
+            <a href='#' className='card'>
+              <img src={comic.thumb} alt={comic.series} />
+              <h2 className='label uppercase text-sm font-semibold'>
+                {comic.series}
+              </h2>
+            </a>
+          </li>
+        ))}
+      </ul>
+
+      <button type='button' className='button uppercase text-xs font-bold'>
+        Load More
+      </button>
+    </div>
+  </section>
+);
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,33 +1,8 @@
-import { comics } from '../data/comics.js';
+import { Comics } from './Comics';
 import './Main.css';
 import { ServicesBar } from './ServicesBar';
 
 const Jumbotron = () => <div className='jumbotron' />;
-
-const Comics = () => (
-  <section className='comics'>
-    <div className='container'>
-      <h1 className='title uppercase text-2xl'>Current Series</h1>
-
-      <ul className='list'>
-        {comics.map((comic) => (
-          <li key={comic.id}>
-            <a href='#' className='card'>
-              <img src={comic.thumb} alt={comic.series} />
-              <h2 className='label uppercase text-sm font-semibold'>
-                {comic.series}
-              </h2>
-            </a>
-          </li>
-        ))}
-      </ul>
-
-      <button type='button' className='button uppercase text-xs font-bold'>
-        Load More
-      </button>
-    </div>
-  </section>
-);
 
 export const Main = () => (
   <main className='main'>
