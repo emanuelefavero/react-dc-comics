50e9c1c7f0607ea31998e1ddcbef76249e1cb065
Emanuele Favero <emafaux89@gmail.com>
2026-07-08 00:42:25 +0200

    refactor: rename class card->comic-card

--- src/components/comics/ComicCard.jsx
+++ src/components/comics/ComicCard.jsx
@@ -1,7 +1,7 @@
 import './ComicCard.css';
 
 export const ComicCard = ({ comic }) => (
-  <a href='#' className='card'>
+  <a href='#' className='comic-card'>
     <img src={comic.thumb} alt={comic.series} />
     <h2 className='label uppercase text-sm font-semibold'>{comic.series}</h2>
   </a>
