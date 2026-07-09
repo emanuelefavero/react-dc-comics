4c96da0d60ca8164141de8c697ce7d306e2527db
Emanuele Favero <emafaux89@gmail.com>
2026-07-10 01:38:46 +0200

    refactor: add conditional rendering in ComicCard component

--- src/components/comics/ComicCard.jsx
+++ src/components/comics/ComicCard.jsx
@@ -1,8 +1,9 @@
 import './ComicCard.css';
 
-export const ComicCard = ({ comic }) => (
-  <a href='#' className='comic-card'>
-    <img src={comic.thumb} alt={comic.series} />
-    <h2 className='label uppercase text-sm font-semibold'>{comic.series}</h2>
-  </a>
-);
+export const ComicCard = ({ comic }) =>
+  comic?.series && (
+    <a href='#' className='comic-card'>
+      <img src={comic.thumb} alt={comic.series} />
+      <h2 className='label uppercase text-sm font-semibold'>{comic.series}</h2>
+    </a>
+  );
